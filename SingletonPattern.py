ab2d74a5c4a9ddec4984193220ca8a50a70845f0
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2022-06-01 16:10:35 +0530

    Update SingletonPattern.py

--- SingletonPattern.py
+++ SingletonPattern.py
@@ -9,7 +9,8 @@ class TransactionProcess:
     # This method is used to get an instance of the class
     @staticmethod
     def getInstance(transType):
-
+        
+        # if the instance does not exists then it creates object and set instance variable to it
         if TransactionProcess.__instance == None:
             TransactionProcess.__instance = TransactionProcess(transType)
 
@@ -17,14 +18,13 @@ class TransactionProcess:
 
 
     def __init__(self,transType):
-
+        
+        # restricting creating of more than one object of this class
         if TransactionProcess.__instance != None:
             raise Exception("Object already exists! Use getInstance method!")
 
         self.transType = transType
-
         TransactionProcess.__instance = self
-
 
 UPI = TransactionProcess("UPI")
 obj = TransactionProcess.getInstance("UPI")
