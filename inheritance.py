e2fe5f2546ee853d1a62fd4b079e8722981f9140
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2022-06-01 09:40:58 +0530

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,18 @@
+"""
+Inheritance
+"""
+
+class Person:
+
+	def __init__(self,name,age):
+		self.name = name
+		self.age = age
+
+class Student(Person):
+
+	def __init__(self,name,age):
+		Person.__init__(self,name,age)
+
+
+student = Student("Shubham",22)
+print("Name :",student.name,"| Age :",student.age)
