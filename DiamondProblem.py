173ed90163d061151f57826a47583cca0efb3bb4
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2024-09-19 19:16:16 +0530

    Update DiamondProblem.py

--- DiamondProblem.py
+++ DiamondProblem.py
@@ -1,27 +1,30 @@
 """
-Diamond problem in multiple inheritance
+The diamond problem occurs when two classes have a common ancestor, 
+and another class has both those classes as base classes, for example:
 """
 
-
 class A:
-	def test(self):
-		print("function inside class A")
-
+    def do_thing(self):
+        print('From A')
 
 class B(A):
-	pass
-	def test(self):
-		print("function inside clas B")
-
+    def do_thing(self):
+        print('From B')
 
 class C(A):
-	def test(self):
-		print("function inside clas C")
-
+    def do_thing(self):
+        print('From C')
 
 class D(B, C):
-	pass
+    pass
 
+d = D()
+d.do_thing()
 
-objD = D()
-objD.test()  # TODO: It will first check class B, then class C and finally will check the class A
+"""
+In some languages, because of how inheritance is implemented, when you call d.do_thing(), 
+it is ambiguous whether you actually want the overridden do_thing from B, or the one from C.
+Python doesn't have this problem because of the method resolution order. 
+Briefly, when you inherit from multiple classes, if their method names conflict, 
+the first one named takes precedence. Since we have specified D(B, C), B.do_thing is called before C.do_thing
+"""
